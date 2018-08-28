805e93de9fe02fe6e4daa46e509678c60f996e7d
Ethan Chen <jonathanchenzheng@hotmail.com>
2018-08-28 14:59:08 -0400

    Second commit

---
+++ main.lua
@@ -0,0 +1,17 @@
+--[[
+    GD50
+    Flappy Bird Remake
+
+    Author: Colton Ogden
+    Student: Ethan Chen
+    
+    cogden@cs50.harvard.edu
+
+    A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple 
+    but effective gameplay mechanic of avoiding pipes indefinitely by just tapping 
+    the screen, making the player's bird avatar flap its wings and move upwards slightly. 
+    A variant of popular games like "Helicopter Game" that floated around the internet
+    for years prior. Illustrates some of the most basic procedural generation of game
+    levels possible as by having pipes stick out of the ground by varying amounts, acting
+    as an infinitely generated obstacle course for the player.
+]]
