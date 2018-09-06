5b01f0d553790f0daf85857c844d9b396c91efe6
Ethan Chen <jonathanchenzheng@hotmail.com>
2018-09-06 13:07:26 -0400

    Fix game state function calls

--- src/main.lua
+++ src/main.lua
@@ -137,57 +137,13 @@ function love.keyboard.wasPressed(key)
 end
 
 function love.update(dt)
-    -- scroll background by preset speed * dt, looping back to 0 after the looping point
-    if scrolling then
-		backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) 
-			% BACKGROUND_LOOPING_POINT
+    -- update background and ground scroll offsets
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % 
+        BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
 
-		-- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
-		groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) 
-			% GROUND_LOOPING_POINT
-
-		spawnTimer = spawnTimer + dt
-
-		-- spawn a new PipePair if the timer is past 2 seconds
-		if spawnTimer > 2 then
-			-- modify the last Y coordinate we placed so pipe gaps aren't too far apart
-			-- no higher than 10 pixels below the top edge of the screen,
-			-- and no lower than a gap length (90 pixels) from the bottom
-			local y = math.max(-PIPE_HEIGHT + 10, 
-				math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-			lastY = y
-        
-			table.insert(pipePairs, PipePair(y))
-			spawnTimer = 0
-		end
-
-		-- update the bird for input and gravity
-		bird:update(dt)
-
-		-- for every pipe in the scene...
-		for k, pair in pairs(pipePairs) do
-			pair:update(dt)
-
-			-- check to see if bird collided with Pipe
-			for l, pipe in pairs(pair.pipes) do
-				if bird:collides(pipe) then
-					-- pause the game to show collision
-					scrolling = false
-				end
-			end
-		end
-
-		-- remove any flagged pipes
-		-- we need this second loop, rather than deleting in the previous loop, because
-		-- modifying the table in-place without explicit keys will result in skipping the
-		-- next pipe, since all implicit keys (numerical indices) are automatically shifted
-		-- down after a table removal
-		for k, pair in pairs(pipePairs) do
-			if pair.remove then
-				table.remove(pipePairs, k)
-			end
-		end
-	end
+    -- now, we just update the state machine, which defers to the right state
+    gStateMachine:update(dt)
 
     -- reset input table
     love.keyboard.keysPressed = {}
@@ -196,20 +152,11 @@ end
 function love.draw()
     push:start()
 
-    -- draw the background at the negative looping point
-    love.graphics.draw(background, -backgroundScroll, 0)
-
-    -- render all the pipe pairs in our scene
-    for k, pair in pairs(pipePairs) do
-        pair:render()
-    end
-
-    -- draw the ground on top of the background, toward the bottom of the screen,
-    -- at its negative looping point
-    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
-
-    -- render our bird to the screen using its own render logic
-    bird:render()
+	-- draw state machine between the background and ground, which defers
+	-- render logic to the currently active state
+	love.graphics.draw(background, - backgroundScroll, 0)
+	gStateMachine:render()
+	love.graphics.draw(ground, - groundScroll, VIRTUAL_HEIGHT - 16)
     
     push:finish()
 end
